0ca9d33637148a69786eccedb32be7727cb63930
Martin Weingart <martinweingart@gmail.com>
2024-03-05 19:38:24 -0300

    Update icons

--- src/components/base/Icon/icons/icons.tsx
+++ src/components/base/Icon/icons/icons.tsx
@@ -15,8 +15,8 @@ const IconPathMap: Record<string, React.ReactNode> = {
   ),
   search: (
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
     />
   ),
@@ -25,6 +25,20 @@ const IconPathMap: Record<string, React.ReactNode> = {
       strokeLinecap="round"
       strokeLinejoin="round"
       d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"
+    />
+  ),
+  clear: (
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M6 18 18 6M6 6l12 12"
+    />
+  ),
+  chevronDown: (
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="m19.5 8.25-7.5 7.5-7.5-7.5"
     />
   ),
 };
