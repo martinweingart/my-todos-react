3bdccefc0f8a540cc102184d80764d3f9e39e6cd
Martin Weingart <martinweingart@gmail.com>
2024-03-09 11:17:01 -0300

    Update components

--- src/components/base/DatePicker/DatePicker.tsx
+++ src/components/base/DatePicker/DatePicker.tsx
@@ -1,7 +1,13 @@
 import { InputBase } from "../InputBase/InputBase";
 
-export interface DatePickerProps {
-  className?: string;
+export interface DatePickerProps
+  extends Omit<
+    React.DetailedHTMLProps<
+      React.InputHTMLAttributes<HTMLInputElement>,
+      HTMLInputElement
+    >,
+    "onChange" | "value"
+  > {
   label?: string;
   value: string;
   onChange: (value: string) => void;
@@ -12,6 +18,7 @@ export const DatePicker = ({
   label,
   value,
   onChange,
+  ...props
 }: DatePickerProps): React.ReactElement<DatePickerProps> => {
   return (
     <InputBase
@@ -19,6 +26,7 @@ export const DatePicker = ({
       label={label}
       renderInput={(className, id) => (
         <input
+          {...props}
           id={id}
           className={className}
           type="date"
--- src/components/base/TextField/TextField.tsx
+++ src/components/base/TextField/TextField.tsx
@@ -1,26 +1,31 @@
+import { forwardRef } from "react";
 import { InputBase } from "../InputBase/InputBase";
 
-export interface TextFieldProps {
-  className?: string;
+export interface TextFieldProps
+  extends Omit<
+    React.DetailedHTMLProps<
+      React.InputHTMLAttributes<HTMLInputElement>,
+      HTMLInputElement
+    >,
+    "onChange" | "value"
+  > {
   label?: string;
   value: string;
-  placeholder?: string;
   onChange: (value: string) => void;
 }
 
-export const TextField = ({
-  className = "",
-  label = "",
-  value,
-  placeholder,
-  onChange,
-}: TextFieldProps): React.ReactElement<TextFieldProps> => {
+export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(function (
+  { className = "", label = "", value, placeholder, onChange, ...props },
+  ref
+): React.ReactElement<TextFieldProps> {
   return (
     <InputBase
       className={className}
       label={label}
       renderInput={(className, id) => (
         <input
+          {...props}
+          ref={ref}
           id={id}
           className={className}
           type="text"
@@ -31,4 +36,4 @@ export const TextField = ({
       )}
     />
   );
-};
+});
