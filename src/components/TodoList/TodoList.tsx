b4aeb602ebe636031ad1b942d6dc99eecf7d863c
Martin Weingart <martinweingart@gmail.com>
2024-03-09 11:19:46 -0300

    Add delete icon to TodoListItem component

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,17 +1,29 @@
+import { useContext } from "react";
 import { Todo } from "../../models/Todo";
 import { TodoListItem } from "./TodoListItem/TodoListItem";
+import { AppContext } from "../../context";
 
-export interface TodoListProps {
-  todos: Todo[];
-}
+export const TodoList = (): React.ReactElement => {
+  const todos = useContext(AppContext).todos;
 
-export const TodoList = ({
-  todos,
-}: TodoListProps): React.ReactElement<TodoListProps> => {
+  const onDelete = (id: number) => {
+    //TODO
+  };
+
+  const onDone = (id: number) => {
+    //TODO
+  };
+
   return (
     <ul className="flex flex-col gap-2  ">
-      {todos.map((todo) => (
-        <TodoListItem description={todo.description} done={todo.done} />
+      {todos.map((todo: Todo) => (
+        <TodoListItem
+          key={`todolist-item-${todo.id}`}
+          description={todo.description}
+          done={todo.done}
+          onDelete={() => onDelete(todo.id)}
+          onDone={() => onDone(todo.id)}
+        />
       ))}
     </ul>
   );
--- src/components/TodoList/TodoListItem/TodoListItem.tsx
+++ src/components/TodoList/TodoListItem/TodoListItem.tsx
@@ -1,20 +1,32 @@
-import { Checkbox } from "../../base";
+import { Checkbox, Icon, IconButton } from "../../base";
 
 export interface TodoListItemProps {
   description: string;
   done: boolean;
+  onDone: () => void;
+  onDelete: () => void;
 }
 
 export const TodoListItem = ({
   description,
   done,
+  onDone,
+  onDelete,
 }: TodoListItemProps): React.ReactElement<TodoListItemProps> => {
   return (
     <li className="flex items-start gap-1 py-2 border-l-4  border-b rounded border-cyan-600">
-      <Checkbox checked={done} onChange={() => {}} />
-      <span className={`mt-1 break-words ${done ? "line-through" : ""}`}>
-        {description}
-      </span>
+      <Checkbox checked={done} onChange={onDone} />
+
+      <div className="flex flex-1 flex-col gap-1">
+        <span className={`mt-1 break-words ${done ? "line-through" : ""}`}>
+          {description}
+        </span>
+      </div>
+
+      <IconButton
+        onClick={onDelete}
+        icon={<Icon className="flex size-6 text-cyan-800" name="trash" />}
+      />
     </li>
   );
 };
