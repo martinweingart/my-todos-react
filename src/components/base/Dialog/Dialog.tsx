23d0cb25256d05546b1763c9bd3edcd0fc50365e
Martin Weingart <martinweingart@gmail.com>
2024-03-09 11:17:46 -0300

    Close dialog on escape

--- src/components/base/Dialog/Dialog.tsx
+++ src/components/base/Dialog/Dialog.tsx
@@ -19,10 +19,18 @@ export const Dialog = ({
 }: DialogProps): React.ReactElement<DialogProps> => {
   const nodeRef = useRef(null);
 
+  const onKeyUp: React.KeyboardEventHandler<HTMLInputElement> = (e) =>
+    e.key === "Escape" && onClose();
+
   return createPortal(
     <Transition in={open} timeout={360} unmountOnExit nodeRef={nodeRef}>
       {(state) => (
-        <div className="fixed inset-0 h-full w-full" ref={nodeRef}>
+        <div
+          className="fixed inset-0 h-full w-full"
+          ref={nodeRef}
+          tabIndex={0}
+          onKeyUp={onKeyUp}
+        >
           <Backdrop state={state} onClick={onClose} />
           <DialogContent state={state} className={className}>
             {children}
